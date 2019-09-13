2750d2ad068ba584b529b1c65d8cd902d6f2cc2a
Maelys <maelys@opuscope.com>
2019-09-13 12:40:24 +0200

    Creation of different resolution versions

---
+++ Input/512_Simple_Texture_Reduction.bat
@@ -0,0 +1,46 @@
+set ffmpegpath=%~dp0..\ffmpeg\bin\ffmpeg.exe
+
+set inputPath=%1
+
+set inputFolder=%~dp0..\Input
+
+
+set image_name=%~n1
+
+set resolution=512
+
+if exist "%1.png" goto:png
+if exist "%1.jpg" goto:jpg
+if exist "%1.jpeg" goto:jpeg
+
+:png
+set image_extension=png
+goto:done
+
+:jpg
+set image_extension=jpg
+goto:done
+
+:jpeg
+set image_extension=jpeg
+goto:done
+
+:done
+echo image_extension is %image_extension%
+
+
+REM use the instruction which keeps the image ratio, because some textures won't be square
+"%ffmpegpath%" -i %1 -vf scale=%resolution%:-1 %image_name%_reduced.%image_extension%
+
+set newimage_path=%1\..\%image_name%_reduced.%image_extension%
+
+echo new image path is %newimage_path%
+
+REM rename original in _old
+ren "%1" "%image_name%_old.%image_extension%"
+
+REM rename reduced in original
+ren "%newimage_path%" "%image_name%.%image_extension%"
+
+
+pause
